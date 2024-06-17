317af883dd9f78242b832f8d93a0ab8d6777c414
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-17 14:35:16 +0900

    Title: 369 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240617/369 게임/369-game.py
@@ -0,0 +1,10 @@
+n=input()
+for i in range(1,int(n)+1):
+    if (i%3==0):
+        print(0,end=" ")
+    elif(i>=10):
+        if ((str(i)[0]=='3' or str(i)[1]=='3') or (str(i)[0]=='6' or str(i)[1]=='6') or 
+        (str(i)[0]=='9' or str(i)[1]=='9')):
+            print(0,end=" ")
+    else:
+        print(i,end=" ")
