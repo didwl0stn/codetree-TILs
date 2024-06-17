e7c35a929082541fd5cb88ad84f08c5226d59382
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-17 14:35:56 +0900

    Title: 369 게임, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/369 게임/369-game.py
+++ 240617/369 게임/369-game.py
@@ -6,5 +6,7 @@ for i in range(1,int(n)+1):
         if ((str(i)[0]=='3' or str(i)[1]=='3') or (str(i)[0]=='6' or str(i)[1]=='6') or 
         (str(i)[0]=='9' or str(i)[1]=='9')):
             print(0,end=" ")
+        else:
+            print(i,end=" ")
     else:
         print(i,end=" ")
