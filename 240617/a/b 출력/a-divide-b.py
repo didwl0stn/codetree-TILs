c1e95f8ce164e762a38783a13b077edd4b0e5907
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-17 13:00:31 +0900

    Title: a/b 출력, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240617/a/b 출력/a-divide-b.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+answer=""
+for i in range(21):
+    if (i == 1):
+        answer+="."
+    answer+=str(a//b)
+    if (a//b==0):
+        a*=10
+    else:
+        a = a%b
+
+print(answer)
