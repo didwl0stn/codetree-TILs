c46998f48c1fc2db7f80b03f4c645d868da86060
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-20 00:11:10 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240620/배열 채우기/filling-array.py
+++ 240620/배열 채우기/filling-array.py
@@ -1,6 +1,9 @@
 arr = list(map(int,input().split()))
-if (arr[-1]==0):
-    arr.pop()
-arr = arr[::-1]
+for i in range(0,len(arr)):
+    if (arr[i]==0):
+        arr=arr[i-1::-1]
+
+    elif(i==len(arr)-1):
+        arr=arr[::-1]
 for i in arr:
     print(i,end=" ")
