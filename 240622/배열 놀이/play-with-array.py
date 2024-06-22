8c18db3ebd5ca95869f30f0268b311e7101d9e1e
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-22 23:31:26 +0900

    Title: 배열 놀이, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240622/배열 놀이/play-with-array.py
@@ -0,0 +1,18 @@
+n,q=map(int,input().split())
+arr = list(map(int,input().split()))
+while(q>0):
+    command = input().split()
+    if len(command)==2:
+        a,elem=command
+        if a=='1':
+            print(arr[int(elem)-1])
+        if a=='2':
+            if (int(elem) in arr):
+                print(arr.index(int(elem))+1)
+            else:
+                print(0)
+    if len(command)==3:
+        a,s,e=command
+        for i in arr[int(s)-1:int(e)]:
+            print(i,end=" ")
+    q-=1
