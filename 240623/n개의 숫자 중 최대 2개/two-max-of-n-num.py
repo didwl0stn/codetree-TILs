07c796dae2bbb35feef82aff946c2c5277f0467f
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-23 20:49:09 +0900

    Title: n개의 숫자 중 최대 2개, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240623/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240623/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -1,6 +1,4 @@
 n=int(input())
 arr=list(map(int,input().split()))
-first_max=max(arr)
-idx=arr.index(first_max)
-arr=arr[:idx]+arr[idx+1:]
-print(first_max,max(arr))
+sorted_arr=sorted(arr,reverse=True)
+print(sorted_arr[0], sorted_arr[1])
