7b986aac39ac50b44f72a743ee7750999266f767
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-24 02:56:45 +0900

    Title: 대각선으로 숫자 채우기, Time: 172ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240624/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,20 @@
+# 입력 받기
+n, m = map(int, input().split())
+
+# 2차원 배열 초기화
+arr = [[0 for _ in range(m)] for _ in range(n)]
+
+# 숫자 채우기
+num = 1
+for k in range(n + m - 1):
+    for i in range(n):
+        for j in range(m):
+            if i + j == k:
+                arr[i][j] = num
+                num += 1
+
+# 배열 출력
+for row in arr:
+    for elem in row:
+        print(elem,end=" ")
+    print("")
