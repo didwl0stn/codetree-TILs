9ef3435dddff35073a00578ba78350702434f2d5
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-24 19:37:20 +0900

    Title: 첫 번째와 두 번째 교환, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240624/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -0,0 +1,9 @@
+string = list(input())
+a=string[0]
+b=string[1]
+for i in range(len(string)):
+    if string[i]==a:
+        string[i]=b
+    elif string[i]==b:
+        string[i]=a
+print("".join(string))
