5152c55efe3741f56ac0674512c3e0cb5d5f8481
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-24 17:17:08 +0900

    Title: 특정 문자의 등장 횟수, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240624/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240624/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,2 +1,9 @@
 string=input()
-print(string.count('ee'), string.count('eb'))
+ee_cnt=0
+eb_cnt=0
+for i in range(len(string)-1):
+    if (string[i]=='ee'[0] and string[i+1]=='ee'[1]):
+        ee_cnt+=1
+    if (string[i]=='eb'[0] and string[i+1]=='eb'[1]):
+        eb_cnt+=1
+print(ee_cnt, eb_cnt)
