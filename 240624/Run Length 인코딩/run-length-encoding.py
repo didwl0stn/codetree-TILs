407f1d036ad9067a9c4f7560c440b5f7a884051b
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-24 15:53:50 +0900

    Title: Run Length 인코딩, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240624/Run Length 인코딩/run-length-encoding.py
+++ 240624/Run Length 인코딩/run-length-encoding.py
@@ -2,15 +2,18 @@ ans=""
 a=input()
 former=a[0]
 cnt=1
-for i in range(1,len(a)):
-    if a[i]==former:
-        cnt+=1
+for i in range(len(a)):
+    if i==0:
+        pass
     else:
-        ans+=former
-        ans+=str(cnt)
-        former=a[i]
-        cnt=1
-    
+        if a[i]==former:
+            cnt+=1
+        else:
+            ans+=former
+            ans+=str(cnt)
+            former=a[i]
+            cnt=1
+        
     if (i==len(a)-1):
         ans+=former
         ans+=str(cnt)
