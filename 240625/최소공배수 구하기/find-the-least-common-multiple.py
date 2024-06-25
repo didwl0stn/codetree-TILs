c832f864bcf3e0732c2764510ce2b9ee7ff0467b
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2024-06-25 14:53:36 +0900

    Title: 최소공배수 구하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240625/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,12 @@
+n,m = map(int,input().split())
+def lcm(n,m):
+    if n<m:
+        n,m=m,n
+    if m==0:
+        return n
+    if n%m==0:
+        return m
+    else:
+        return lcm(m,n%m)
+
+print(n*m//lcm(n,m))
