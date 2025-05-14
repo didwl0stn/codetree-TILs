a156771af9302782dacc213f3deb9dfc9bb50cdd
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-05-14 11:19:32 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250514/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int choo = 13;
+    double weight = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << choo << " * " << weight << " = " << choo*weight;
+    return 0;
+}
