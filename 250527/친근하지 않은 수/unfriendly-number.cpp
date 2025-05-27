b33b3126306dd283a430de857b19aec9db0186f1
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-05-27 17:26:47 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/친근하지 않은 수/unfriendly-number.cpp
+++ 250527/친근하지 않은 수/unfriendly-number.cpp
@@ -6,7 +6,7 @@ int main() {
     int n;
     cin >> n;
     int count = 0;
-    for (int i=1; i<n; i++) {
+    for (int i=1; i<=n; i++) {
         if (i%2 == 0 || i%3 ==0 || i%5 ==0) {
             continue;
         }
