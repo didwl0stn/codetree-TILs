f7252fda00059f219f93a02b25dc1d09ff558720
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-05-27 17:26:09 +0900

    Title: 친근하지 않은 수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250527/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i=1; i<n; i++) {
+        if (i%2 == 0 || i%3 ==0 || i%5 ==0) {
+            continue;
+        }
+        else {
+            count +=1;
+        }
+    }
+    cout << count;
+
+    return 0;
+}
