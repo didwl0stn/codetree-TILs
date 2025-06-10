4844c56956e938c062cec0a877601aa6f9ede7aa
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-10 12:23:42 +0900

    Title: 길이 단위 환산하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250610/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin >> n;
+    cout << fixed;
+    cout.precision(1);
+    cout << 30.48*n;
+    return 0;
+}
