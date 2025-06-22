e0ab048ac8bfc90d6d8f9be105d377ef1ffa4027
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-22 10:44:00 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250622/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    char d;
+    cin >> a>>d>>b>>d>>c;
+    cout << 0 <<a<<d<<c<<d<<b;
+    return 0;
+}
