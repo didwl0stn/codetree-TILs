fcc357bbc725bcf7a1a91a0012488aaffa956360
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-22 10:14:50 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250622/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a,b;
+    cout.precision(2);
+    cout << fixed;
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
