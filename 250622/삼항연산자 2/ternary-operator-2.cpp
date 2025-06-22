152d1aec83ed6176e988a00394926063bae5afbc
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-22 13:07:49 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250622/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if (a==1)
+        cout << "t";
+    else 
+        cout << "f";
+    return 0;
+}
