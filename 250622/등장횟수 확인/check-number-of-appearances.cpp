822ea510006040deb58c9baa144203d5b565fedb
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-22 19:49:29 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250622/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt=0;
+    for (int i=0; i<5; i++) {
+        int temp;
+        cin >> temp;
+        if (temp%2==0)
+            cnt += 1;
+    }
+    cout << cnt;
+    return 0;
+}
