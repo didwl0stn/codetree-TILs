febff62b09417ec6f54cabbe09c5e911052a6261
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-23 13:54:44 +0900

    Title: 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250623/개수 세기/count-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin >> n >> m;
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        int temp;
+        cin >>temp;
+        if (temp==m) 
+            cnt+=1;
+    }
+    cout << cnt;
+    return 0;
+}
