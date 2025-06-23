5638a36981f1404c1e865505ca5b8814c4e2c943
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-23 17:51:00 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250623/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        if (i%2==1) {
+            cout << "*\n";
+        }
+        else {
+            for (int j=0; j<i; j++) {
+                cout << "* ";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
