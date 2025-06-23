167825be8d011302a1b1d079a63801b9d09e90a5
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-23 09:34:47 +0900

    Title: 10개 입력받기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250623/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    int sums = 0;
+    cout << fixed;
+    cout.precision(1);
+    for (int i=0; i<10; i++) {
+        cin >> arr[i];
+        if (arr[i]==0) {
+            cout << sums << " " << (double)sums/i;
+            break;
+        }
+        sums +=arr[i];
+        if (i==9) {
+            cout << sums << " " << (double)sums/10;
+        }
+
+    }
+    return 0;
+}
