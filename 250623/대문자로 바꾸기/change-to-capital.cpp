5d10d740d881f830d6587b00f32ef592134aa0ae
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-23 23:05:37 +0900

    Title: 대문자로 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250623/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[5][3];
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<3; j++) {
+            cin >> arr[i][j];
+            arr[i][j] = (char)((int)arr[i][j]-32);
+        }
+    }
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<3; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
