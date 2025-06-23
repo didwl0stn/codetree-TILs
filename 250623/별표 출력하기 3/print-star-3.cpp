a9813852b4ac3eed4a0447cbe5b6e68447f274ca
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-23 17:13:35 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250623/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        for (int k=0; k<i; k++) {
+            cout << "  ";
+        }
+        for (int j=0; j<n*2-(i*2+1); j++) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
