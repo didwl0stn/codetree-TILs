956e608084edd240a6848a1e6d135693763b0b64
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 11:25:17 +0900

    Title: 문자열에서 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250624/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >>str;
+    for (int i=0; i<str.length(); i++) {
+        cout << str[i] << "\n";
+    }
+    return 0;
+}
