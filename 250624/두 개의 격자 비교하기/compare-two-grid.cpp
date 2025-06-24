ca207810ef817237702799264c8fcdcca9599b3c
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 09:50:59 +0900

    Title: 두 개의 격자 비교하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin >> n >> m;
+    int arr1[10][10] = {};
+    int arr2[10][10] = {};
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> arr1[i][j];
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (arr1[i][j] == arr2[i][j]) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << 1 << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
