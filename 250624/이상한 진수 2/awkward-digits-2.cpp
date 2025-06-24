277535e75eef46bf20daadd5135a6f9a26a041a1
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 13:26:06 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250624/이상한 진수 2/awkward-digits-2.cpp
+++ 250624/이상한 진수 2/awkward-digits-2.cpp
@@ -1,3 +1,36 @@
+// #include <iostream>
+// #include <string>
+// #include <cmath>
+// using namespace std;
+
+// string a;
+
+// int main() {
+//     cin >> a;
+
+//     // Please write your code here.
+//     for (int i=0; i<a.length(); i++) {
+//         if (a[i]=='0') {
+//             a[i]='1';
+//             break;
+//         }
+//     }
+
+//     int temp = a.length();
+
+//     int res = 0;
+//     for (int i =temp-1; i>=0; i--) {
+//         int val = a[temp-i-1] - '0';
+        
+//         if (val != 0)
+//             res += pow(val*2,i);
+        
+     
+//     }
+//     cout << res;
+
+//     return 0;
+// }
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -8,26 +41,21 @@ string a;
 int main() {
     cin >> a;
 
-    // Please write your code here.
-    for (int i=0; i<a.length(); i++) {
-        if (a[i]=='0') {
-            a[i]='1';
+    // 첫 번째 0을 1로 바꿈
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] == '0') {
+            a[i] = '1';
             break;
         }
     }
 
-    int temp = a.length();
-
     int res = 0;
-    for (int i =temp-1; i>=0; i--) {
-        int val = a[temp-i-1] - '0';
-        
-        if (val != 0)
-            res += pow(val*2,i);
-        
-     
+    int len = a.length();
+    for (int i = 0; i < len; i++) {
+        int val = a[i] - '0';
+        res += val * pow(2, len - 1 - i);  // 2진수 계산
     }
-    cout << res;
 
+    cout << res;
     return 0;
 }
