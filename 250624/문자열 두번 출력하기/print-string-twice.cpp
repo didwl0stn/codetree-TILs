f2059a953e0b37506206140ce9dec194248a30e2
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 10:36:06 +0900

    Title: 문자열 두번 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250624/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    cout << str << "\n" << str;
+    return 0;
+}
