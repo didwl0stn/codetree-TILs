6cd0a00434edfbee1f4f9bddce3bc4fffa610c09
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 13:27:55 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250624/이상한 진수 2/awkward-digits-2.cpp
+++ 250624/이상한 진수 2/awkward-digits-2.cpp
@@ -1,3 +1,36 @@
+// // #include <iostream>
+// // #include <string>
+// // #include <cmath>
+// // using namespace std;
+
+// // string a;
+
+// // int main() {
+// //     cin >> a;
+
+// //     // Please write your code here.
+// //     for (int i=0; i<a.length(); i++) {
+// //         if (a[i]=='0') {
+// //             a[i]='1';
+// //             break;
+// //         }
+// //     }
+
+// //     int temp = a.length();
+
+// //     int res = 0;
+// //     for (int i =temp-1; i>=0; i--) {
+// //         int val = a[temp-i-1] - '0';
+        
+// //         if (val != 0)
+// //             res += pow(val*2,i);
+        
+     
+// //     }
+// //     cout << res;
+
+// //     return 0;
+// // }
 // #include <iostream>
 // #include <string>
 // #include <cmath>
@@ -8,40 +41,34 @@
 // int main() {
 //     cin >> a;
 
-//     // Please write your code here.
-//     for (int i=0; i<a.length(); i++) {
-//         if (a[i]=='0') {
-//             a[i]='1';
+//     // 첫 번째 0을 1로 바꿈
+//     for (int i = 0; i < a.length(); i++) {
+//         if (a[i] == '0') {
+//             a[i] = '1';
 //             break;
 //         }
 //     }
 
-//     int temp = a.length();
-
 //     int res = 0;
-//     for (int i =temp-1; i>=0; i--) {
-//         int val = a[temp-i-1] - '0';
-        
-//         if (val != 0)
-//             res += pow(val*2,i);
-        
-     
+//     int len = a.length();
+//     for (int i = 0; i < len; i++) {
+//         int val = a[i] - '0';
+//         res += val * pow(2, len - 1 - i);  // 2진수 계산
 //     }
-//     cout << res;
 
+//     cout << res;
 //     return 0;
 // }
+
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
-string a;
-
 int main() {
+    string a;
     cin >> a;
 
-    // 첫 번째 0을 1로 바꿈
+    // 왼쪽부터 처음 만나는 '0'을 '1'로 바꾸기
     for (int i = 0; i < a.length(); i++) {
         if (a[i] == '0') {
             a[i] = '1';
@@ -49,11 +76,10 @@ int main() {
         }
     }
 
+    // 이진수를 10진수로 변환
     int res = 0;
-    int len = a.length();
-    for (int i = 0; i < len; i++) {
-        int val = a[i] - '0';
-        res += val * pow(2, len - 1 - i);  // 2진수 계산
+    for (int i = 0; i < a.length(); i++) {
+        res = res * 2 + (a[i] - '0');
     }
 
     cout << res;
