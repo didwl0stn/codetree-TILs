d5ab10f708352583c67ddf5002f0ab21de72f462
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-06-24 11:30:18 +0900

    Title: 문자열 추가하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250624/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    str.append("Hello");
+    cout << str;
+    return 0;
+}
