86b060985417eaec8fafeb0b3330e305eb030e4e
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-07-02 12:15:08 +0900

    Title: 비오는 날, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250702/비오는 날/rainy-day.py
@@ -0,0 +1,27 @@
+n = int(input())
+date = []
+day = []
+weather = []
+
+for _ in range(n):
+    d, dy, w = input().split()
+    date.append(d)
+    day.append(dy)
+    weather.append(w)
+
+# Please write your code here.
+
+class Temp:
+    def __init__(self,date,day,weather):
+        self.data = date
+        self.day=day
+        self.weather=weather
+
+ans=[]
+for i in range(n):
+    ans.append(Temp(date[i],day[i],weather[i]))
+
+for elem in ans:
+    if (elem.weather=='Rain'):
+        print(elem.data,elem.day,elem.weather)
+        break
