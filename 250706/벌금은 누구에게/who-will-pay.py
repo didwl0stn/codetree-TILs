89257c9d2de1b0ebc1217336a3b799cfddf87751
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-07-06 15:09:37 +0900

    Title: 벌금은 누구에게, Time: 68ms, Memory: 16MB, Status: Passed - Codetree

--- 250706/벌금은 누구에게/who-will-pay.py
+++ 250706/벌금은 누구에게/who-will-pay.py
@@ -4,9 +4,10 @@ student = [int(input()) for _ in range(M)]
 # Please write your code here.
 
 count=[0] * N
-for i in student:
-    
+ans=-1
+for i in student:   
     count[i-1]+=1
     if (count[i-1]>=K):
-        print(i)
+        ans=i
         break
+print(ans)
