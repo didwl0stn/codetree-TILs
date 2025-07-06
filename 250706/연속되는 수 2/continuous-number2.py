429948cb74bd5d46145ec1651dbccc2197756307
didwl0stn <44148447+didwl0stn@users.noreply.github.com>
2025-07-06 10:16:27 +0900

    Title: 연속되는 수 2, Time: 66ms, Memory: 16MB, Status: Passed - Codetree

--- 250706/연속되는 수 2/continuous-number2.py
+++ 250706/연속되는 수 2/continuous-number2.py
@@ -6,10 +6,11 @@ now = arr[0]
 cnt = 1
 maxs=1
 for i in range(1,len(arr)):
-    if i==len(arr)-1:
-        maxs=max(cnt,maxs)
+    
     if now==arr[i]:
         cnt +=1
+        if i==len(arr)-1:
+            maxs=max(cnt,maxs)
     else:
         maxs = max(cnt,maxs)
         now=arr[i]
